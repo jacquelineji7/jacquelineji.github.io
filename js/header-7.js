1e26c26c07b65edef9d73f7976c8fc1bc7da02c9
Jacqueline Ji <46661488+jacquelineji7@users.noreply.github.com>
2021-09-13 03:26:02 -0400

    Add files via upload

--- js/header-7.js
+++ js/header-7.js
@@ -1,6 +1,26 @@
 let navToggle = document.querySelector(".nav__toggle");
 let navWrapper = document.querySelector(".nav__wrapper");
 
+
+
+const nav = document.querySelector("#my-nav")
+    let lastScrollY = window.scrollY;
+
+
+    window.addEventListener("scroll", () => {
+      if (window.scrollY <= 80 ){
+
+      }else if(lastScrollY < window.scrollY){
+        nav.classList.add("nav--hidden");
+      }else{
+        nav.classList.remove("nav--hidden");
+      }
+
+      lastScrollY = window.scrollY;
+    } );
+
+
+
 navToggle.addEventListener("click", function () {
   if (navWrapper.classList.contains("active")) {
     this.setAttribute("aria-expanded", "false");
